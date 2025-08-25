4b71990fcb39fb1ff9e18c1c3ae6f82151655007
Said Sulayman <105841619+SaidSulayman@users.noreply.github.com>
2025-08-25 11:31:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+      // Mobile menu toggle functionality
+        document.getElementById('menuToggle').addEventListener('click', function() {
+            document.getElementById('navMenu').classList.toggle('active');
+        });
